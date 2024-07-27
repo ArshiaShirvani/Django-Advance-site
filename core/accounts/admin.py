3277a0805043d579669e087c11dd686270aa9962
Arshia <arshiashirvani.f1385@gmail.com>
2024-07-28 00:37:54 +0330

    I wrote Profile class and added signal between profile and User
    I wrote Profile class and added signal between Profile and User
    define custom user and user manager in admin.py
    and define create_user and super user in user manager

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User,Profile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model=User
+    list_display=('email', 'is_superuser', 'is_active',)
+    list_filter=('email', 'is_active',)
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authenticaation',{
+            "fields":(
+                'email','password'
+            ),
+        }),
+        ('Permission',{
+            "fields":(
+                'is_staff','is_active','is_superuser'
+            ),
+        }),
+        ('group permissions',{
+            "fields":(
+                'groups','user_permissions',
+            ),
+        }),
+        ('Important Date',{
+            "fields":(
+                'last_login',
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "is_superuser",
+            )}
+        ),
+    )
+
+admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
